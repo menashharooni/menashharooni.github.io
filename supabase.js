bb5a5d6291a4001801079b1d48cbda82f39b18b7
menashharooni <josephsberkshire@gmail.com>
2026-02-18 22:56:24 -0500

    Delete supabase.js

--- supabase.js
+++
@@ -1,8 +0,0 @@
-const SUPABASE_URL = "https://zrtzbzzyanfvovstxrgc.supabase.co";
-const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpydHpienp5YW5mdm92c3R4cmdjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE0NjE2NTYsImV4cCI6MjA4NzAzNzY1Nn0.iJ9CO6h-EqOCWTLRrsgJx77n7kg5CzpQMhdqWOljNnE";
-
-const supabaseClient = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
-);
